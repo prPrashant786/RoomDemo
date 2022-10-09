487a42c4d2dc7efef1dc604b8600bd91f4dbaedf
Prashant <pr.prashant0786@gmail.com>
2022-10-09 21:36:09 +0530

    All UI Added

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.roomdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.example.roomdemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btadd?.setOnClickListener {
+            //TODO add record
+        }
+    }
+
+    fun addRecord(studentDao: StudentDao){
+        val name = binding?.etname?.text.toString()
+        val email = binding?.etemail?.text.toString()
+
+        if (email.isNotEmpty() && name.isNotEmpty()){
+            lifecycleScope.launch {
+
+                studentDao.insert(StudentEntity(name = name, email = email))
+
+                Toast.makeText(applicationContext,"DATA SAVED",Toast.LENGTH_LONG).show()
+                binding?.etname?.text?.clear()
+                binding?.etemail?.text?.clear()
+            }
+        }else{
+            Toast.makeText(applicationContext,"ENTER THE DATA",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/example/roomdemo/StudentDatabase.kt
+++ app/src/main/java/com/example/roomdemo/StudentDatabase.kt
@@ -1,13 +1,37 @@
 package com.example.roomdemo
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Entity
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [StudentEntity::class], version = 1)
 abstract class StudentDatabase:RoomDatabase() {
 
-    abstract fun studentDao() : StudentDao
+    abstract fun studentDao() : StudentDao //Dao Class
+
+    companion object    { // It allowed add function to student data
+        @Volatile
+        private var INSTANCE : StudentDatabase? = null;
+
+        fun getInstance(context: Context):StudentDatabase{
+            synchronized(this){
+                var instance = INSTANCE;
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        StudentDatabase::class.java,
+                        "employee_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 
 
 }
